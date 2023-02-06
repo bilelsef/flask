8fbcffa56aaf4403d89e41eeb47c20201fe81fb9
bilelsef <64594378+bilelsef@users.noreply.github.com>
2023-02-06 10:44:46 +0100

    Update user.py

--- src/api/models/user.py
+++ src/api/models/user.py
@@ -9,7 +9,7 @@ class User(db.Model):
     annonces = db.relationship("Annonce", backref="owner", lazy="dynamic")
     role = db.Column(db.String(1),default="1")
     def toJson(self):
-        return {"id" : self.id,"full_name":self.full_name,"address":None ,"email":self.email,"picture_link":self.picture_link,"phone_number":self.info.phone_number,"annonces":list(map(lambda annonce:annonce.toJson(),self.annonces))}
+        return {"id" : self.id,"full_name":self.full_name,"address":self.info.address ,"email":self.email,"picture_link":self.picture_link,"phone_number":self.info.phone_number,"annonces":list(map(lambda annonce:annonce.toJson(),self.annonces))}
     def add(self):
         db.session.add(self)
         db.session.commit()
